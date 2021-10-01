39de9e545df12aa541ac1d10a21065fed5a41257
nabroleonx <nabroleonx@gmail.com>
2021-10-01 23:29:10 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,321 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+import { Fragment, useState } from "react";
+import { Dialog, Transition, Tab } from "@headlessui/react";
+import {
+  CalendarIcon,
+  CogIcon,
+  HomeIcon,
+  MapIcon,
+  MenuIcon,
+  SearchCircleIcon,
+  SpeakerphoneIcon,
+  UserGroupIcon,
+  ViewGridAddIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+import {
+  ChevronLeftIcon,
+  FilterIcon,
+  MailIcon,
+  PhoneIcon,
+  SearchIcon,
+} from "@heroicons/react/solid";
+import Projects from "./Components/Projects";
+import Certifications from "./Components/Certifications";
+import About from "./Components/About";
 
-function App() {
+const user = {
+  name: "Tom Cook",
+  imageUrl:
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
+const navigation = [
+  { name: "Dashboard", href: "#", icon: HomeIcon, current: false },
+  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
+  { name: "Teams", href: "#", icon: UserGroupIcon, current: false },
+  { name: "Directory", href: "#", icon: SearchCircleIcon, current: true },
+  { name: "Announcements", href: "#", icon: SpeakerphoneIcon, current: false },
+  { name: "Office Map", href: "#", icon: MapIcon, current: false },
+];
+const secondaryNavigation = [
+  { name: "Apps", href: "#", icon: ViewGridAddIcon },
+  { name: "Settings", href: "#", icon: CogIcon },
+];
+const tabs = [
+  { name: "Profile", href: "#", current: true },
+  { name: "Calendar", href: "#", current: false },
+  { name: "Recognition", href: "#", current: false },
+];
+const profile = {
+  name: "Ricardo Cooper",
+  imageUrl:
+    "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+  coverImageUrl:
+    "https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+  about: `
+    <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
+    <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
+  `,
+  fields: {
+    Phone: "(555) 123-4567",
+    Email: "ricardocooper@example.com",
+    Title: "Senior Front-End Developer",
+    Team: "Product Development",
+    Location: "San Francisco",
+    Sits: "Oasis, 4th floor",
+    Salary: "$145,000",
+    Birthday: "June 8, 1990",
+  },
+};
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  let [categories] = useState({
+    About: <About />,
+    Projects: <Projects />,
+    Certifications: <Certifications />,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="relative h-screen flex overflow-hidden bg-white">
+      <Transition.Root show={sidebarOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 flex z-40 lg:hidden"
+          onClose={setSidebarOpen}
         >
-          Learn React
-        </a>
-      </header>
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+          </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <div className="relative flex-1 flex flex-col max-w-xs w-full bg-white focus:outline-none">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-in-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in-out duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="absolute top-0 right-0 -mr-12 pt-2">
+                  <button
+                    type="button"
+                    className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    onClick={() => setSidebarOpen(false)}
+                  >
+                    <span className="sr-only">Close sidebar</span>
+                    <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                  </button>
+                </div>
+              </Transition.Child>
+
+              <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
+                <a href="#" className="flex-shrink-0 group block">
+                  <div className="flex items-center">
+                    <div>
+                      <img
+                        className="inline-block h-10 w-10 rounded-full"
+                        src={user.imageUrl}
+                        alt=""
+                      />
+                    </div>
+                    <div className="ml-3">
+                      <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
+                        {user.name}
+                      </p>
+                      <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
+                        View profile
+                      </p>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </Transition.Child>
+          <div className="flex-shrink-0 w-14" aria-hidden="true">
+            {/* Force sidebar to shrink to fit close icon */}
+          </div>
+        </Dialog>
+      </Transition.Root>
+
+      <div className="flex flex-col min-w-0 flex-1 overflow-hidden">
+        <div className="lg:hidden">
+          <div className="flex items-center justify-between bg-gray-50 border-b border-gray-200 px-4 py-1.5">
+            <div>
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark-pink-500.svg"
+                alt="Workflow"
+              />
+            </div>
+            <div>
+              <button
+                type="button"
+                className="-mr-3 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-pink-600"
+                onClick={() => setSidebarOpen(true)}
+              >
+                <span className="sr-only">Open sidebar</span>
+                <MenuIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 relative z-0 flex overflow-hidden">
+          <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
+            {/* Breadcrumb */}
+            <nav
+              className="flex items-start px-4 py-3 sm:px-6 lg:px-8 xl:hidden"
+              aria-label="Breadcrumb"
+            >
+              <a
+                href="#"
+                className="inline-flex items-center space-x-3 text-sm font-medium text-gray-900"
+              >
+                <ChevronLeftIcon
+                  className="-ml-2 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                <span>Directory</span>
+              </a>
+            </nav>
+
+            <article>
+              {/* Profile header */}
+              <div>
+                <div>
+                  <img
+                    className="h-32 w-full object-cover lg:h-48"
+                    src={profile.coverImageUrl}
+                    alt=""
+                  />
+                </div>
+                <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+                  <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+                    <div className="flex">
+                      <img
+                        className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+                        src={profile.imageUrl}
+                        alt=""
+                      />
+                    </div>
+                    <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                      <div className="sm:hidden 2xl:block mt-6 min-w-0 flex-1">
+                        <h1 className="text-2xl font-bold text-gray-900 truncate">
+                          {profile.name}
+                        </h1>
+                      </div>
+                      <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
+                        <button
+                          type="button"
+                          className="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+                        >
+                          <MailIcon
+                            className="-ml-1 mr-2 h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                          <span>Message</span>
+                        </button>
+                        <button
+                          type="button"
+                          className="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+                        >
+                          <PhoneIcon
+                            className="-ml-1 mr-2 h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                          <span>Call</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="hidden sm:block 2xl:hidden mt-6 min-w-0 flex-1">
+                    <h1 className="text-2xl font-bold text-gray-900 truncate">
+                      {profile.name}
+                    </h1>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-6 sm:mt-2 2xl:mt-5">
+                <div className="border-b border-gray-200">
+                  <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <Tab.Group>
+                      <Tab.List className="flex p-1 space-x-1 bg-blue-900/20 rounded-xl">
+                        {Object.keys(categories).map((category) => (
+                          <Tab
+                            key={category}
+                            className={({ selected }) =>
+                              classNames(
+                                "w-full py-2.5 text-sm leading-5 font-medium",
+                                "focus:outline-none",
+                                selected
+                                  ? "border-teal-400 border-b text-teal-500"
+                                  : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+                              )
+                            }
+                          >
+                            {category}
+                          </Tab>
+                        ))}
+                      </Tab.List>
+                      <Tab.Panels className="mt-2">
+                        {Object.values(categories).map((posts) => (
+                          <Tab.Panel
+                            key={posts}
+                            className={classNames(
+                              "bg-white rounded-xl p-3",
+                              "focus:outline-none"
+                            )}
+                          >
+                            {posts}
+                          </Tab.Panel>
+                        ))}
+                      </Tab.Panels>
+                    </Tab.Group>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
